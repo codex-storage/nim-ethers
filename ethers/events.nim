c49a9cbae949d20e4ebc91f5a9ccf27f3c26b64a
Mark Spanbroek <mark@spanbroek.net>
2022-03-29 10:26:31 +0200

    Decode indexed event parameters bytes1 up to bytes32

--- ethers/events.nim
+++ ethers/events.nim
@@ -8,6 +8,17 @@ type
   ValueType = uint8 | uint16 | uint32 | uint64 | UInt256 | UInt128 |
               int8 | int16 | int32 | int64 | Int256 | Int128 |
               bool | Address
+  SmallByteArray = array[ 1, byte] | array[ 2, byte] | array[ 3, byte] |
+                   array[ 4, byte] | array[ 5, byte] | array[ 6, byte] |
+                   array[ 7, byte] | array[ 8, byte] | array[ 9, byte] |
+                   array[10, byte] | array[11, byte] | array[12, byte] |
+                   array[13, byte] | array[14, byte] | array[15, byte] |
+                   array[16, byte] | array[17, byte] | array[18, byte] |
+                   array[19, byte] | array[20, byte] | array[21, byte] |
+                   array[22, byte] | array[23, byte] | array[24, byte] |
+                   array[25, byte] | array[26, byte] | array[27, byte] |
+                   array[28, byte] | array[29, byte] | array[30, byte] |
+                   array[31, byte] | array[32, byte]
 
 push: {.upraises: [].}
 
@@ -27,7 +38,7 @@ func decode*[E: Event](_: type E, data: seq[byte], topics: seq[Topic]): ?!E =
     if field.hasCustomPragma(indexed):
       if i >= topics.len:
         return failure "indexed event parameter not found"
-      if typeof(field) is ValueType:
+      if typeof(field) is ValueType or typeof(field) is SmallByteArray:
         field = ?AbiDecoder.decode(@(topics[i]), typeof(field))
       inc i
   success event
