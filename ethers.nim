dc3b4774e14994df8da153a3b441d389e70f8573
Jackie <jackie_weiss@web.de>
2022-07-12 20:26:39 +0200

    Include wallet in library

--- ethers.nim
+++ ethers.nim
@@ -2,8 +2,10 @@ import ./ethers/provider
 import ./ethers/signer
 import ./ethers/providers/jsonrpc
 import ./ethers/contract
+import ./ethers/wallet
 
 export provider
 export signer
 export jsonrpc
 export contract
+export wallet
