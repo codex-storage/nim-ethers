e8592bb92219f5fcec25af22bed474a4f95c0a54
Mark Spanbroek <mark@spanbroek.net>
2022-09-20 15:36:42 +0200

    Remove unnecessary error check

--- ethers/testing.nim
+++ ethers/testing.nim
@@ -1,23 +1,19 @@
 import std/strutils
-import std/json
 import ./provider
 
 proc revertReason*(e: ref ProviderError): string =
-  try:
-    var msg = e.msg
-    const revertPrefixes = @[
-      # hardhat
-      "Error: VM Exception while processing transaction: reverted with " &
-      "reason string ",
-      # ganache
-      "VM Exception while processing transaction: revert "
-    ]
-    for prefix in revertPrefixes.items:
-      msg = msg.replace(prefix)
-    msg = msg.replace("\'")
-    return msg
-  except JsonParsingError:
-    return ""
+  var msg = e.msg
+  const revertPrefixes = @[
+    # hardhat
+    "Error: VM Exception while processing transaction: reverted with " &
+    "reason string ",
+    # ganache
+    "VM Exception while processing transaction: revert "
+  ]
+  for prefix in revertPrefixes.items:
+    msg = msg.replace(prefix)
+  msg = msg.replace("\'")
+  return msg
 
 proc reverts*[T](call: Future[T]): Future[bool] {.async.} =
   try:
