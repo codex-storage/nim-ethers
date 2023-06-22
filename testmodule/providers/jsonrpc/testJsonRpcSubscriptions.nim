a7dc0ac9eb0880eb7a48194135e471d6413cf3b1
Mark Spanbroek <mark@spanbroek.net>
2023-06-22 12:47:19 +0200

    Move subscription handling to its own module

---
+++ testmodule/providers/jsonrpc/testJsonRpcSubscriptions.nim
@@ -0,0 +1,38 @@
+import std/json
+import pkg/asynctest
+import pkg/json_rpc/rpcclient
+import ethers/provider
+import ethers/providers/jsonrpc/subscriptions
+
+suite "JsonRpcSubscriptions":
+
+  test "can be instantiated with an http client":
+    let client = newRpcHttpClient()
+    let subscriptions = JsonRpcSubscriptions.new(client)
+    check not isNil subscriptions
+
+  test "can be instantiated with a websocket client":
+    let client = newRpcWebSocketClient()
+    let subscriptions = JsonRpcSubscriptions.new(client)
+    check not isNil subscriptions
+
+suite "Web socket subscriptions":
+
+  var subscriptions: JsonRpcSubscriptions
+  var client: RpcWebSocketClient
+
+  setup:
+    client = newRpcWebSocketClient()
+    await client.connect("ws://localhost:8545")
+    subscriptions = JsonRpcSubscriptions.new(client)
+
+  test "subscribes to new blocks":
+    var latestBlock: Block
+    proc callback(blck: Block) {.async.} =
+      latestBlock = blck
+    let subscription = await subscriptions.subscribeBlocks(callback)
+    discard await client.call("evm_mine", newJArray())
+    check eventually(latestBlock.number.isSome)
+    check latestBlock.hash.isSome
+    check latestBlock.timestamp > 0.u256
+    await subscription.unsubscribe()
