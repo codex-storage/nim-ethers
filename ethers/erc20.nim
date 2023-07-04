469d4b165f63b8179cd33657c9d67797a489a08e
Mark Spanbroek <mark@spanbroek.net>
2023-07-04 15:44:07 +0200

    Formatting

--- ethers/erc20.nim
+++ ethers/erc20.nim
@@ -17,36 +17,46 @@ type
     spender* {.indexed.}: Address
     value*: UInt256
 
-method name*(erc20: Erc20Token): string {.base, contract, view.}
-## Returns the name of the token.
+method name*(token: Erc20Token): string {.base, contract, view.}
+  ## Returns the name of the token.
 
 method symbol*(token: Erc20Token): string {.base, contract, view.}
-## Returns the symbol of the token, usually a shorter version of the name.
+  ## Returns the symbol of the token, usually a shorter version of the name.
 
 method decimals*(token: Erc20Token): uint8 {.base, contract, view.}
-## Returns the number of decimals used to get its user representation.
-## For example, if `decimals` equals `2`, a balance of `505` tokens should
-## be displayed to a user as `5.05` (`505 / 10 ** 2`).
+  ## Returns the number of decimals used to get its user representation.
+  ## For example, if `decimals` equals `2`, a balance of `505` tokens should
+  ## be displayed to a user as `5.05` (`505 / 10 ** 2`).
 
-method totalSupply*(erc20: Erc20Token): UInt256 {.base, contract, view.}
-## Returns the amount of tokens in existence.
+method totalSupply*(token: Erc20Token): UInt256 {.base, contract, view.}
+  ## Returns the amount of tokens in existence.
 
-method balanceOf*(erc20: Erc20Token, account: Address): UInt256 {.base, contract, view.}
-## Returns the amount of tokens owned by `account`.
+method balanceOf*(token: Erc20Token,
+                  account: Address): UInt256 {.base, contract, view.}
+  ## Returns the amount of tokens owned by `account`.
 
-method allowance*(erc20: Erc20Token, owner, spender: Address): UInt256 {.base, contract, view.}
-## Returns the remaining number of tokens that `spender` will be allowed
-## to spend on behalf of `owner` through {transferFrom}. This is zero by default.
-##
-## This value changes when {approve} or {transferFrom} are called.
+method allowance*(token: Erc20Token,
+                  owner: Address,
+                  spender: Address): UInt256 {.base, contract, view.}
+  ## Returns the remaining number of tokens that `spender` will be allowed
+  ## to spend on behalf of `owner` through {transferFrom}. This is zero by
+  ## default.
+  ##
+  ## This value changes when {approve} or {transferFrom} are called.
 
-method transfer*(erc20: Erc20Token, recipient: Address, amount: UInt256) {.base, contract.}
-## Moves `amount` tokens from the caller's account to `recipient`.
+method transfer*(token: Erc20Token,
+                 recipient: Address,
+                 amount: UInt256) {.base, contract.}
+  ## Moves `amount` tokens from the caller's account to `recipient`.
 
-method approve*(token: Erc20Token, spender: Address, amount: UInt256) {.base, contract.}
-## Sets `amount` as the allowance of `spender` over the caller's tokens.
+method approve*(token: Erc20Token,
+                spender: Address,
+                amount: UInt256) {.base, contract.}
+  ## Sets `amount` as the allowance of `spender` over the caller's tokens.
 
-method transferFrom*(erc20: Erc20Token, spender: Address, recipient: Address, amount: UInt256) {.base, contract.}
-## Moves `amount` tokens from `from` to `to` using the allowance
-## mechanism. `amount` is then deducted from the caller's allowance.
-
+method transferFrom*(token: Erc20Token,
+                     spender: Address,
+                     recipient: Address,
+                     amount: UInt256) {.base, contract.}
+  ## Moves `amount` tokens from `from` to `to` using the allowance
+  ## mechanism. `amount` is then deducted from the caller's allowance.
