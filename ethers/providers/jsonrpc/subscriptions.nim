64cc99442dfb5326dbb18eaff62e880f18cd65f5
Eric <5089238+emizzle@users.noreply.github.com>
2024-01-30 19:25:24 +1100

    support nim 2.0

--- ethers/providers/jsonrpc/subscriptions.nim
+++ ethers/providers/jsonrpc/subscriptions.nim
@@ -49,6 +49,22 @@ proc setMethodHandler(
 ) =
   subscriptions.methodHandlers[`method`] = handler
 
+when (NimMajor) >= 2:
+  proc hash*[A](x: openArray[A]): Hash {.raises: [].} =
+    ## Efficient hashing of arrays and sequences.
+    ## There must be a `hash` proc defined for the element type `A`.
+    when A is byte:
+      result = murmurHash(x)
+    elif A is char:
+      when nimvm:
+        result = hashVmImplChar(x, 0, x.high)
+      else:
+        result = murmurHash(toOpenArrayByte(x, 0, x.high))
+    else:
+      for a in x:
+        result = result !& hash(a)
+      result = !$result
+
 func hash*(n: OrderedTable[string, JsonNode]): Hash {.raises: [].}
 
 proc hash*(n: JsonNode): Hash {.noSideEffect, raises: [].} =
