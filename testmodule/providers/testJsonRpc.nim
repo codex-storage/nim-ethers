d46beb531310f1c258895612bbcc7d100c6db729
Mark Spanbroek <mark@spanbroek.net>
2024-03-18 16:02:39 +0100

    jsonrpc: extract error data from JSON RPC error
    Inspired by 'spelunk' from ethers.js:
    https://github.com/ethers-io/ethers.js/blob/f97b92bbb1bde22fcc44100af78d7f31602863ab/packages/providers/src.ts/json-rpc-provider.ts#L25

--- testmodule/providers/testJsonRpc.nim
+++ testmodule/providers/testJsonRpc.nim
@@ -2,5 +2,6 @@ import ./jsonrpc/testJsonRpcProvider
 import ./jsonrpc/testJsonRpcSigner
 import ./jsonrpc/testJsonRpcSubscriptions
 import ./jsonrpc/testConversions
+import ./jsonrpc/testErrors
 
 {.warning[UnusedImport]:off.}
