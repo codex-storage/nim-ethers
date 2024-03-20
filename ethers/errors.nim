9c76803302910153d09de9334c3dd9c3155aa93a
Mark Spanbroek <mark@spanbroek.net>
2024-03-20 11:24:53 +0100

    support custom errors with arguments

--- ethers/errors.nim
+++ ethers/errors.nim
@@ -1,3 +1,4 @@
+import pkg/contractabi
 import pkg/contractabi/selector
 import ./basics
 
@@ -7,14 +8,33 @@ type SolidityError* = object of EthersError
 
 template errors*(types) {.pragma.}
 
+func selector(E: type): FunctionSelector =
+  when compiles(E.arguments):
+    selector($E, typeof(E.arguments))
+  else:
+    selector($E, tuple[])
+
+func matchesSelector(E: type, data: seq[byte]): bool =
+  const selector = E.selector.toArray
+  data.len >= 4 and selector[0..<4] == data[0..<4]
+
+func decodeArguments(E: type, data: seq[byte]): auto =
+  AbiDecoder.decode(data[4..^1], E.arguments)
+
 func decode*[E: SolidityError](_: type E, data: seq[byte]): ?!(ref E) =
-  const name = $E
-  const selector = selector(name, typeof(()))
-  if data.len < 4:
-    return failure "unable to decode " & name & ": signature too short"
-  if selector.toArray[0..<4] != data[0..<4]:
-    return failure "unable to decode " & name & ": signature doesn't match"
-  success (ref E)()
+  if not E.matchesSelector(data):
+    return failure "unable to decode " & $E & ": selector doesn't match"
+  when compiles(E.arguments):
+    without arguments =? E.decodeArguments(data), error:
+      return failure "unable to decode " & $E & ": " & error.msg
+    success (ref E)(arguments: arguments)
+  else:
+    success (ref E)()
+
+func encode*[E: SolidityError](_: type AbiEncoder, error: ref E): seq[byte] =
+  result = @(E.selector.toArray)
+  when compiles(error.arguments):
+    result &= AbiEncoder.encode(error.arguments)
 
 template convertCustomErrors*[ErrorTypes: tuple](body: untyped): untyped =
   try:
