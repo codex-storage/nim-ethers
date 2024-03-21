fc52ffa3e963a8e9d2c6d173cbaf01d6570d9674
Mark Spanbroek <mark@spanbroek.net>
2024-03-21 09:32:15 +0100

    handle custom errors in confirm() calls

---
+++ ethers/errors/conversion.nim
@@ -0,0 +1,12 @@
+import ../basics
+import ../provider
+import ./encoding
+
+func customErrorConversion*(ErrorTypes: type tuple): ConvertCustomErrors =
+  func convert(error: ref ProviderError): ref EthersError =
+    if data =? error.data:
+      for e in ErrorTypes.default.fields:
+        if error =? typeof(e).decode(data):
+          return error
+    return error
+  convert
