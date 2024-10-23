f8c196843d61bddb7e96540b05edeb984f9ed3b8
Eric <5089238+emizzle@users.noreply.github.com>
2024-10-23 18:54:44 +1100

    update port range

--- testmodule/providers/jsonrpc/rpc_mock.nim
+++ testmodule/providers/jsonrpc/rpc_mock.nim
@@ -14,7 +14,7 @@ type MockRpcHttpServer* = ref object
   srv: RpcHttpServer
 
 proc new*(_: type MockRpcHttpServer): MockRpcHttpServer =
-  let port = rand(65000..<66000)
+  let port = rand(49152..65535)
   let srv = newRpcHttpServer(["127.0.0.1:" & $port])
   let filters = initTable[string, bool]()
   MockRpcHttpServer(filters: filters, newFilterCounter: 0, srv: srv)
