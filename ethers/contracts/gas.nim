3484755c6affa82b0e46da002a22abaf9878d5d4
Mark Spanbroek <mark@spanbroek.net>
2025-04-10 09:47:16 +0200

    Handle custom errors when estimating gas

--- ethers/contracts/gas.nim
+++ ethers/contracts/gas.nim
@@ -5,6 +5,7 @@ import ../signer
 import ./contract
 import ./contractcall
 import ./transactions
+import ./errors
 import ./syntax
 
 type ContractGasEstimations[C] = distinct C
@@ -42,4 +43,4 @@ func createGasEstimationCall*(procedure: NimNode): NimNode =
   result.addAsyncPragma()
   result.addUsedPragma()
   result.addEstimateCall()
-
+  result.addErrorHandling()
